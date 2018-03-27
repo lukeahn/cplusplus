802fcf170755816552dea43966fabf317665c58c
Luke Ahn <ahnnuga@gmail.com>
2018-03-27 12:45:59 -0400

    Initial Commit

---
+++ cplusplus/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cplusplus
+//
+//  Created by Luke Ahn on 3/27/18.
+//  Copyright © 2018 Luke Ahn. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
